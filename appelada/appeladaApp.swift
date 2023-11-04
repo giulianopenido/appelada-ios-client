0a969a6b530d813e8dd0a010d17cb45f0e293580
Giuliano Penido <giuliano.junior@localiza.com>
2023-11-04 16:48:58 -0300

    Initial Commit

---
+++ appelada/appeladaApp.swift
@@ -0,0 +1,17 @@
+//
+//  appeladaApp.swift
+//  appelada
+//
+//  Created by MAC-00093 on 04/11/23.
+//
+
+import SwiftUI
+
+@main
+struct appeladaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
